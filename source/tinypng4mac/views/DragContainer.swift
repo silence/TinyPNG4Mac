ea315b9e03ddafd4a82489f51ddf7a85fbffba50
william <pjwwgl@126.com>
2018-03-27 16:53:14 +0800

    修改支持拖拽目录

--- source/tinypng4mac/views/DragContainer.swift
+++ source/tinypng4mac/views/DragContainer.swift
@@ -25,7 +25,7 @@ class DragContainer: NSView {
 	
 	override func awakeFromNib() {
 		super.awakeFromNib()
-		
+
 		self.register(forDraggedTypes: [NSFilenamesPboardType, NSURLPboardType, NSPasteboardTypeTIFF]);
 	}
 	
@@ -35,14 +35,16 @@ class DragContainer: NSView {
 	
 	override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
 //		self.layer?.backgroundColor = NSColor(white: highlightColor, alpha: 1).CGColor;
-		let res = checkExtension(sender)
+        
+        //这里不做扩展检查，支持查找整个目录
+//        let res = checkExtension(sender)
 		if let delegate = self.delegate {
 			delegate.draggingEntered();
 		}
-		if res {
+//        if res {
 			return NSDragOperation.generic
-		}
-		return NSDragOperation()
+//        }
+//        return NSDragOperation()
 	}
 	
 	override func draggingExited(_ sender: NSDraggingInfo?) {
@@ -60,14 +62,36 @@ class DragContainer: NSView {
 	override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
 		var files = Array<URL>()
 		if let board = sender.draggingPasteboard().propertyList(forType: NSFilenamesPboardType) as? NSArray {
+            
+            let manager = FileManager.default
+            
 			for path in board {
 				let url = URL(fileURLWithPath: path as! String)
-				let fileExtension = url.pathExtension.lowercased()
-				if acceptTypes.contains(fileExtension) {
-					files.append(url)
-				}
+                
+                if self.isFolder(path as! String) {
+                    //查找目录下所有的资源文件
+//                    let directorys = try? manager.contentsOfDirectory(atPath: path as! String)
+                    let enumeratorAtURLValues = manager.enumerator(at: url, includingPropertiesForKeys: nil, options: .skipsHiddenFiles, errorHandler: nil)?.allObjects
+//                    debugPrint("enumeratorAtURL: \(String(describing: enumeratorAtURL?.allObjects))");
+                    
+                    for path1 in enumeratorAtURLValues! {
+                        let fileExtension = (path1 as! URL).pathExtension.lowercased()
+                        if acceptTypes.contains(fileExtension) {
+                            files.append(path1 as! URL)
+                        }
+                    }
+                }
+                else
+                {
+                    let fileExtension = url.pathExtension.lowercased()
+                    if acceptTypes.contains(fileExtension) {
+                        files.append(url)
+                    }
+                }
 			}
 		}
+        
+        debugPrint(String(describing: files))
 		
 		if self.delegate != nil {
 			self.delegate?.draggingFileAccept(files);
@@ -75,6 +99,14 @@ class DragContainer: NSView {
 		
 		return true
 	}
+    
+    func isFolder(_ path: String) -> Bool {
+        
+        var isFolder:ObjCBool = false;
+        
+        let manager = FileManager.default
+        return manager.fileExists(atPath: path, isDirectory:&isFolder);
+    }
 	
 	func checkExtension(_ draggingInfo: NSDraggingInfo) -> Bool {
 		if let board = draggingInfo.draggingPasteboard().propertyList(forType: NSFilenamesPboardType) as? NSArray {
